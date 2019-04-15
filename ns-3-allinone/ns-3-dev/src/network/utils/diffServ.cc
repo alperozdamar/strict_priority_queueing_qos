ebba7a00f3bd5ed34ea518bd86772d8b87c23d5c
Rozita <rozita.teim@gmail.com>
2019-04-15 00:14:54 -0700

    WIP on diffServ

--- ns-3-allinone/ns-3-dev/src/network/utils/diffServ.cc
+++ ns-3-allinone/ns-3-dev/src/network/utils/diffServ.cc
@@ -3,68 +3,77 @@
 #include "ns3/uinteger.h"
 #include "diffServ.h"
 
+
 namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE ("DiffServ");
 NS_OBJECT_ENSURE_REGISTERED (DiffServ);
 
-TypeId DiffServ::GetTypeId (void) {
-    static TypeId tid = TypeId ("ns3::DiffServ")
-    SetParent<Queue> ()
-    .SetGroupName("Network")
-    .AddAttribute ("Mode", "Whether to use bytes (see MaxBytes) or packets (see MaxPackets) as the maximum queue size metric.", EnumValue (QUEUE_MODE_PACKETS),
-        MakeEnumAccessor (&DropTailQueue::SetMode,&DropTailQueue::GetMode),
-        MakeEnumChecker (QUEUE_MODE_BYTES, "QUEUE_MODE_BYTES", QUEUE_MODE_PACKETS, "QUEUE_MODE_PACKETS"))
-    .AddAttribute ("MaxPackets", "The maximum number of packets accepted by this DropTailQueue.",UintegerValue (100), MakeUintegerAccessor (&DropTailQueue::m_maxPackets),MakeUintegerChecker<uint32_t> ())
-    .AddAttribute ("MaxBytes", "The maximum number of bytes accepted by this DropTailQueue.", UintegerValue (100 * 65535), MakeUintegerAccessor (&DropTailQueue::m_maxBytes), MakeUintegerChecker<uint32_t> ());
+// TypeId DiffServ::GetTypeId (void) 
+// {
+//     static TypeId tid = TypeId ("ns3::DiffServ");
+//     SetParent<Queue> ()
+//         .SetGroupName("Network")
+//         .AddConstructor<DiffServ> ()
+//         .AddAttribute ("Mode", 
+//                        "Whether to use bytes (see MaxBytes) or packets (see MaxPackets) as the maximum queue size metric.", 
+//                         EnumValue (QUEUE_MODE_PACKETS), 
+//                         MakeEnumAccessor (&DiffServ::SetMode,&DiffServ::GetMode),
+//                         MakeEnumChecker (QUEUE_MODE_BYTES, "QUEUE_MODE_BYTES", QUEUE_MODE_PACKETS, "QUEUE_MODE_PACKETS"))
+//         .AddAttribute ("MaxPackets", "The maximum number of packets accepted by this DiffServe.",UintegerValue (100), 
+//                         MakeUintegerAccessor (&DiffServ::m_maxPackets),MakeUintegerChecker<uint32_t> ())
+//         .AddAttribute ("MaxBytes", "The maximum number of bytes accepted by this DiffServ.", UintegerValue (100 * 65535), 
+//                         MakeUintegerAccessor (&DiffServ::m_maxBytes), MakeUintegerChecker<uint32_t> ())
+//     ;
 
-    return tid;
+//     return tid;
+// }
+
+DiffServ::DiffServ () :
+    Queue (),
+    m_packets (),
+    m_bytesInQueue (0)
+{
+    NS_LOG_FUNCTION (this);
 }
 
-DropTailQueue::DropTailQueue () :
-Queue (),
-m_packets (),
-m_bytesInQueue (0)
+DiffServ::~DiffServ ()
 {
-}
-
-DropTailQueue::~DropTailQueue ()
-{
-NS_LOG_FUNCTION (this);
+    NS_LOG_FUNCTION (this);
 }
 
 void
-DropTailQueue::SetMode (DropTailQueue::QueueMode mode)
+DiffServ::SetMode (DiffServ::QueueMode mode)
 {
-NS_LOG_FUNCTION (this << mode);
-m_mode = mode;
+    NS_LOG_FUNCTION (this << mode);
+    m_mode = mode;
 }
  
-DropTailQueue::QueueMode
-DropTailQueue::GetMode (void) const
+DiffServ::QueueMode
+DiffServ::GetMode (void) const
 {
-NS_LOG_FUNCTION (this);
-return m_mode;
+    NS_LOG_FUNCTION (this);
+    return m_mode;
 }
 
 bool 
-DropTailQueue::DoEnqueue (Ptr<Packet> p)
+DiffServ::DoEnqueue (Ptr<ns3::Packet> p)
 {
-NS_LOG_FUNCTION (this << p);
+    NS_LOG_FUNCTION (this << p);
 
-if (m_mode == QUEUE_MODE_PACKETS && (m_packets.size () >= m_maxPackets))
-{
-NS_LOG_LOGIC ("Queue full (at max packets) -- droppping pkt");
-Drop (p);
-return false;
-}
+    if (m_mode == QUEUE_MODE_PACKETS && (m_packets.size () >= m_maxPackets))
+    {
+        NS_LOG_LOGIC ("Queue full (at max packets) -- droppping pkt");
+        DropBeforeEnqueue (p);
+        return false;
+    }   
 
-if (m_mode == QUEUE_MODE_BYTES && (m_bytesInQueue + p->GetSize () >= m_maxBytes))
-{
- NS_LOG_LOGIC ("Queue full (packet would exceed max bytes) -- droppping pkt");
-Drop (p);
-return false;
-}
+    if (m_mode == QUEUE_MODE_BYTES && (m_bytesInQueue + p->GetSize () >= m_maxBytes))
+    {
+        NS_LOG_LOGIC ("Queue full (packet would exceed max bytes) -- droppping pkt");
+        DropBeforeEnqueue (p);
+        return false;
+    }
 
     m_bytesInQueue += p->GetSize ();
     m_packets.push (p);
@@ -73,17 +82,17 @@ return false;
     NS_LOG_LOGIC ("Number bytes " << m_bytesInQueue);
 
     return true;
-    }
+}
 
-    Ptr<Packet>
-    DropTailQueue::DoDequeue (void)
-    {
+Ptr<ns3::Packet>
+DiffServ::DoDequeue (void)
+{
     NS_LOG_FUNCTION (this);
 
     if (m_packets.empty ())
     {
-    NS_LOG_LOGIC ("Queue empty");
-    return 0;
+        NS_LOG_LOGIC ("Queue empty");
+        return 0;
     }
 
     Ptr<Packet> p = m_packets.front ();
@@ -98,22 +107,49 @@ return false;
     return p;
  }
 
-  Ptr<const Packet>
-  DropTailQueue::DoPeek (void) const
-    {
+Ptr<const ns3::Packet>
+DiffServ::DoPeek (void) const
+{
     NS_LOG_FUNCTION (this);
 
     if (m_packets.empty ())
     {
-     NS_LOG_LOGIC ("Queue empty");
-    return 0;
-     }
+        NS_LOG_LOGIC ("Queue empty");
+        return 0;
+    }
     Ptr<Packet> p = m_packets.front ();
 
-        NS_LOG_LOGIC ("Number packets " << m_packets.size ());
-     NS_LOG_LOGIC ("Number bytes " << m_bytesInQueue);
+    NS_LOG_LOGIC ("Number packets " << m_packets.size ());
+    NS_LOG_LOGIC ("Number bytes " << m_bytesInQueue);
 
     return p;
+}
+
+uint32_t
+DiffServ::Classify (Ptr<ns3::Packet> p) 
+{
+    NS_LOG_FUNCTION (this);
+
+    uint32_t flow;
+    q_class.priority_level = 0;
+
+    if (match(p))
+    {
+        q_class.priority_level = 1;
+    } else{
+        q_class.priority_level = 2;
     }
+
+    return q_class.priority_level;
+}
+
+Ptr<ns3::Packet> 
+DiffServ::Schedule (q_class) 
+{
+    NS_LOG_FUNCTION (this);
+
+    //TODO
+    return DoPeek();
+}
 } // namespace ns3
   
--- ns-3-allinone/ns-3-dev/src/network/utils/diffServ.h
+++ ns-3-allinone/ns-3-dev/src/network/utils/diffServ.h
@@ -11,33 +11,46 @@
 #include <queue>
 #include "ns3/packet.h"
 #include "ns3/queue.h"
+#include "TrafficClass.h"
 
 
 namespace ns3 {
 
 class TraceContainer;
-class DiffServ : public Queue {
+class DiffServ : public Queue<ns3::Packet>{
 public:
 
+  enum QueueMode
+  {
+    QUEUE_MODE_PACKETS,
+    QUEUE_MODE_BYTES,
+  };
   DiffServ();
   virtual ~DiffServ();
-  static TypeId GetTypeId (void); 
   void SetMode (DiffServ::QueueMode mode);
   DiffServ::QueueMode GetMode (void) const;
   Ptr<ns3::Packet> Schedule ();
-  uint32_t Classify (Ptr<ns3::Packet>);
+  uint32_t Classify (Ptr<ns3::Packet> p);
 
+  //TODO : Remove if not needed
+  static TypeId GetTypeId (void); 
 
 private: 
 
+  QueueMode m_mode; 
+  vector<TrafficClass*> q_class;  
+
   virtual bool DoEnqueue (Ptr<ns3::Packet> p);
   virtual Ptr<ns3::Packet> DoDequeue (void);
+  virtual Ptr<ns3::Packet> DoRemove (void);
   virtual Ptr<const ns3::Packet> DoPeek (void) const;
+  
+  
+  //TODO : Remove if not needed
   std::queue<Ptr<ns3::Packet>> m_packets; 
   uint32_t m_maxPackets;              
   uint32_t m_maxBytes;                
-  uint32_t m_bytesInQueue;            
-  QueueMode m_mode;                   
+  uint32_t m_bytesInQueue;  
 
 }; 
 } // namespace ns3
