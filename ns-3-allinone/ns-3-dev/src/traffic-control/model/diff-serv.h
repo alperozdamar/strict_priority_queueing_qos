c1ccda08c8203c5c54bd0a6feee4cf3fca0cdc61
Alper <alper.ozdamar@gmail.com>
2019-04-21 21:00:30 -0700

    diff-serv template conversion..

--- ns-3-allinone/ns-3-dev/src/traffic-control/model/diff-serv.h
+++ ns-3-allinone/ns-3-dev/src/traffic-control/model/diff-serv.h
@@ -17,6 +17,7 @@
 namespace ns3 {
 
 class TraceContainer;
+template<typename Packet>
 class DiffServ : public Queue<ns3::Packet>{
 public:
 
@@ -25,6 +26,7 @@ public:
     QUEUE_MODE_PACKETS,
     QUEUE_MODE_BYTES,
   };
+
   DiffServ();
   virtual ~DiffServ();
   void SetMode (DiffServ::QueueMode mode);
@@ -50,8 +52,128 @@ private:
   //uint32_t m_maxPackets;              
   //uint32_t m_maxBytes;                
   //uint32_t m_bytesInQueue;  
+  NS_LOG_TEMPLATE_DECLARE;
+
+  
 
 }; 
+
+template <typename Packet>
+DiffServ<Packet>::DiffServ () :
+    Queue ()    
+{
+    //NS_LOG_FUNCTION (this);
+}
+
+template <typename Packet>
+DiffServ<Packet>::~DiffServ ()
+{
+   // NS_LOG_FUNCTION (this);
+}
+
+
+template <typename Packet>
+void
+DiffServ<Packet>::SetMode (DiffServ::QueueMode mode)
+{
+    //NS_LOG_FUNCTION (this << mode);
+    m_mode = mode;
+}
+ 
+template <typename Packet>
+DiffServ<Packet>::QueueMode
+DiffServ<Packet>::GetMode (void) const
+{
+    //NS_LOG_FUNCTION (this);
+    return m_mode;
+}
+
+
+//We should call TRaffic Class's Enqueu??? 
+template <typename Packet>
+bool
+DiffServ<Packet>::DoEnqueue (Ptr<ns3::Packet> p)
+{
+    //NS_LOG_FUNCTION (this << p);
+
+    auto iter = q_class.begin();
+      for(;iter!=q_class.end();iter++){ 
+         // std::cout<<*iter<<" ";    
+            if((**iter).match(p)){
+                return (**iter).Enqueue(p);
+            }else{
+
+            } 
+    }       
+    return false;
+}
+ 
+template <typename Packet>
+Ptr<ns3::Packet>
+DiffServ<Packet>::DoDequeue (void)
+{
+    //NS_LOG_FUNCTION (this);
+    auto iter = q_class.begin();
+    for(;iter!=q_class.end();iter++){                      
+        //undefined reference to `ns3::TrafficClass::Dequeue()'         
+        return (**iter).Dequeue();   
+    }    
+    return 0;     
+ }
+
+ 
+template <typename Packet>
+Ptr<const ns3::Packet>
+DiffServ<Packet>::DoPeek (void) const
+{
+   // NS_LOG_FUNCTION (this);  
+    auto iter = q_class.begin();
+      for(;iter!=q_class.end();iter++){ 
+         // std::cout<<*iter<<" ";    
+            if((**iter).Peek()!=0){ //NULL check!
+                return (**iter).Peek();                   
+            }
+      }   
+    return 0;
+}
+
+template <typename Packet>
+Ptr<ns3::Packet> 
+DiffServ<Packet>::DoRemove (void) 
+{
+    //NS_LOG_FUNCTION (this);
+    auto iter = q_class.begin();
+      for(;iter!=q_class.end();iter++){ 
+         // std::cout<<*iter<<" ";    
+            if((**iter).Peek()!=0){ //NULL check!
+                return (**iter).Remove();                    
+            }
+      }        
+    return 0;
+}
+
+template <typename Packet>
+uint32_t
+DiffServ<Packet>::Classify (Ptr<ns3::Packet> p) 
+{
+
+    //NS_LOG_FUNCTION (this)
+
+   if(DoEnqueue(p)){
+       return 1;
+   }else{
+       return 0;
+   }
+}
+
+template <typename Packet>
+Ptr<ns3::Packet> 
+DiffServ<Packet>::Schedule () 
+{
+ //   NS_LOG_FUNCTION (this);
+    return DoDequeue();
+}
+extern template class DiffServ<Packet>;
 } // namespace ns3
 
 #endif /* DIFFSERV_H */
