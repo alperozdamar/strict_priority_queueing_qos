7929190f141f725d7a5fa7e361f2d44801bf402e
Rozita <rozita.teim@gmail.com>
2019-04-24 22:11:04 -0700

    Add source port number

---
+++ ns-3-allinone/ns-3-dev/src/traffic-control/model/source-port-number.cc
@@ -0,0 +1,49 @@
+
+#include "source-port-number.h"
+#include "ns3/packet.h"
+#include "ns3/log.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/udp-header.h"
+
+namespace ns3{
+
+    NS_LOG_COMPONENT_DEFINE ("SourcePortNumber");
+    NS_OBJECT_ENSURE_REGISTERED (SourcePortNumber);
+
+    TypeId
+    SourcePortNumber::GetTypeId (void)
+    {
+        static TypeId tid = TypeId ("ns3::SourcePortNumber")
+                .SetParent<FilterElement> ()
+                .SetGroupName ("trafficControl")
+        ;
+        return tid;
+    }
+
+    SourcePortNumber::SourcePortNumber (uint32_t val)
+    {
+        value = val;
+    }
+
+
+    SourcePortNumber::~SourcePortNumber()
+    {
+        NS_LOG_FUNCTION (this);
+    }
+
+    bool 
+    SourcePortNumber::match (Ptr<Packet> p)
+    {
+        NS_LOG_FUNCTION (this);
+	    Ptr<Packet> copyPacket = p -> Copy();
+	    Ipv4Header ipv4Header;
+	    UdpHeader udpHeader;
+	    copyPacket -> RemoveHeader(ipv4Header);
+	    copyPacket -> RemoveHeader(udpHeader);
+	    uint32_t destinationPort = udpHeader.GetSourcePort();
+	    return destinationPort == value;
+    }
+
+}
+
